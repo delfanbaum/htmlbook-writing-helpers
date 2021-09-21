93ef5c4fac3d1707260db59332405ee8e5f83367
Danny Elfanbaum <delfanbaum@oreilly.com>
2021-09-21 15:06:15 -0400

    inital work

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,8 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
@@ -10,16 +12,42 @@ export function activate(context: vscode.ExtensionContext) {
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "htmlbook-writing-helpers" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('htmlbook-writing-helpers.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from htmlbook-writing-helpers!');
+	let pasteWrapLink = vscode.commands.registerCommand('htmlbook-writing-helpers.pasteWrapLink', () => {
+		vscode.env.clipboard.readText().then((clipboard) => {
+			const editor = vscode.window.activeTextEditor;
+			if (editor && editor.selection){
+				const document = editor.document;
+				editor.edit(editBuilder => {
+					editor.selections.forEach(sel => {
+						var text = editor.document.getText(sel);
+						if (/htt(p|ps):\/\/(.*?)/.test(clipboard)){
+							text = `<a href="${clipboard}">${text}</a>`;
+							editBuilder.replace(sel, text);
+						} else {
+							vscode.window.showInformationMessage("Error: You tried to paste-wrap something other than a link.");
+						}
+					});
+				});
+			}
+		});
 	});
-
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(pasteWrapLink);
+	
+	let pasteXref = vscode.commands.registerCommand('htmlbook-writing-helpers.pasteXref', () => {
+		vscode.env.clipboard.readText().then((clipboard) => {
+			const editor = vscode.window.activeTextEditor;
+			if (editor && editor.selection){
+				const document = editor.document;
+				editor.edit(editBuilder => {
+					editor.selections.forEach(sel => {
+						var text = `<a data-type="xref" href="#${clipboard}">#${clipboard}</a>`;
+						editBuilder.replace(sel, text);
+					});
+				});
+			}
+		});
+	});
+	context.subscriptions.push(pasteXref);
 }
 
 // this method is called when your extension is deactivated
